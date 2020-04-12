831b0096af50e0a2552a8d7728cf2046c4b6ca08
PilYeong <pilyeooong@gmail.com>
2020-04-12 22:00:47 +0900

    ADD: accounts,instagram 앱 생성 및 포스트리스트 API 생성

---
+++ backend/accounts/models.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
+from django.db import models
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.validators import RegexValidator
+from django.shortcuts import resolve_url
+
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = 'Male', '남성'  # DB 저장이 앞, 보여지는부분 뒤
+        FEMALE = 'Female', '여성'
+
+    follower_set = models.ManyToManyField('self', blank=True)
+    following_set = models.ManyToManyField('self', blank=True)
+    website_url = models.URLField(blank=True)
+    bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13,
+                                    validators=[RegexValidator(r'^010-?[1-9]\d{3}-?\d{4}$')], blank=True)
+    gender = models.CharField(max_length=10, choices=GenderChoices.choices, blank=True)
+    avatar = models.ImageField(blank=True, upload_to='accounts/avatar/%Y/%m/%d',
+                               help_text='48px * 48px의 크기의 png/jpg 파일을 업로드 해주세요')
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        else:
+            return resolve_url('pydenticon_image', self.username)
---
+++ backend/instagram/views.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ModelViewSet
+
+from django.shortcuts import render
+
+from .models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [AllowAny]  #FIXME: 인증 적용
