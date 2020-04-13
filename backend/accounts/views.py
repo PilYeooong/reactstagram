8005795502f5f635318e4c9b52316440e1368777
PilYeong <pilyeooong@gmail.com>
2020-04-13 14:40:27 +0900

    ADD: signup api 및 회원가입 폼

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import AllowAny
+from .serializers import SignupSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+User = get_user_model()
+
+
+class SignupView(CreateAPIView):
+    model = User
+    serializer_class = SignupSerializer
+    permission_classes = [
+        AllowAny
+    ]
